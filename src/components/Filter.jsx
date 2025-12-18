724205b6bc4d5305efc2716d74c6d5e2e060dda5
aniketgadekar04 <156211209+aniketgadekar04@users.noreply.github.com>
2025-12-18 12:47:43 +0530

    active button color change

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,6 @@
 // import { BiCategory } from "react-icons/bi";
 
 function Filter({ fiterData, category, setCategory }) {
-  
   function clickHand(title) {
     setCategory(title);
   }
@@ -10,7 +9,13 @@ function Filter({ fiterData, category, setCategory }) {
     <div className="w-11/12 flex flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center">
       {fiterData.map((e) => (
         <button
-          className={`text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-black/50 border-2 transition-all duration-300`}
+          className={`text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-black/50 border-2 transition-all duration-300
+            ${
+              category === e.title
+                ? "bg-opacity-60 border-white"
+                : "bg-opacity-40 border-transparent"
+            }
+            `}
           key={e.id}
           onClick={() => clickHand(e.title)}
         >
@@ -18,7 +23,7 @@ function Filter({ fiterData, category, setCategory }) {
         </button>
       ))}
 
-      {/* ${category===data.title?"bg-opacity-60 border-white":"bg-opacity-40 border-transparent"} */}
+      {/*  */}
     </div>
   );
 }
